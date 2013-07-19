db84d53d5ffd7c151ec76023b27394098881238d
carlh <cghinkle427@gmail.com>
2013-07-19 22:37:53 +0000

    Added new favicon

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
 
 app.get('/', function(request, response) {
     var buffer = fs.readFileSync('public/src/index.html');
